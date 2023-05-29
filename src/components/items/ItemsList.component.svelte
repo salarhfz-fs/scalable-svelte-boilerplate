1ef4be10beef10c14825eefbeb663e88e8bec51a
Salar Hafezi <salar.hfz.work@gmail.com>
2023-05-29 09:51:24 +0330

    Add isSelected toggle handler

--- src/components/items/ItemsList.component.svelte
+++ src/components/items/ItemsList.component.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
     import type { IItem } from '../../models/items/Item.interface';
     export let items: IItem[] = [];
+
+    function handleClick(item: IItem) {
+        item.isSelected = !item.isSelected;
+        items = items;
+        console.log('handleClick', item.id, item.isSelected);
+    }
 </script>
 
 <div>
     <h3>Items</h3>
     <ul>
         {#each items as item}
-            <li>{item.name}</li>
+            <li on:click={() => handleClick(item)}>{item.name} [{item.isSelected}]</li>
         {/each}
     </ul>
 </div>
