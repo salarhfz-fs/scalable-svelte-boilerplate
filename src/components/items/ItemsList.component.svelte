d2470aa5460bbf864ef98a8f6195c9e85e9d4cf3
Salar Hafezi <salar.hfz.work@gmail.com>
2023-05-29 09:18:20 +0330

    Add ItemsList component

---
+++ src/components/items/ItemsList.component.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let items: any[] = [];
+</script>
+
+<div>
+    <h3>Items</h3>
+    <ul>
+        {#each items as item}
+            <li>{item.name}</li>
+        {/each}
+    </ul>
+</div>
+
